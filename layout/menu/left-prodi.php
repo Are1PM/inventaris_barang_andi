7766f3d338fb511a37a23a27e8af3a6c72b749f2
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-13 23:59:17 +0800

    #Mengatur menu prodi

--- layout/menu/left-prodi.php
+++ layout/menu/left-prodi.php
@@ -1,43 +1,29 @@
 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
-    <?php if (isset($_SESSION['username']) && isset($_SESSION['akses'])) : ?>
-        <?php if (isset($_SESSION['username']) && isset($_SESSION['akses'])) : ?>
-            <div class="menu_section">
-                <h3>General</h3>
-                <ul class="nav side-menu">
-                    <li>
-                        <a href="index.php?page=beranda"><i class="fa fa-home"></i> Beranda</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=lihat-barang"><i class="fa fa-edit"></i> Master Barang</a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=lihat-prodi"><i class="fa fa-bar-chart-o"></i> Master Prodi </a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=lihat-kategori"><i class="fa fa-desktop"></i> Master Kategori </a>
-                    </li>
-                    <li>
-                        <a href="index.php?page=lihat-ruangan"><i class="fa fa-table"></i> Master Ruangan </a>
-                    </li>
+    <div class="menu_section">
+        <h3>General</h3>
+        <ul class="nav side-menu">
+            <li>
+                <a href="index.php?page=beranda"><i class="fa fa-home"></i> Beranda</a>
+            </li>
+            <li>
+                <a href="index.php?page=lihat-barang"><i class="fa fa-edit"></i> Master Barang</a>
+            </li>
+            <li>
+                <a href="index.php?page=lihat-prodi"><i class="fa fa-bar-chart-o"></i> Master Prodi </a>
+            </li>
+            <li>
+                <a href="index.php?page=lihat-kategori"><i class="fa fa-desktop"></i> Master Kategori </a>
+            </li>
+            <li>
+                <a href="index.php?page=lihat-ruangan"><i class="fa fa-table"></i> Master Ruangan </a>
+            </li>
 
-                    <li>
-                        <a href="index.php?page=lihat-satuan"><i class="fa fa-clone"></i>Master Satuan </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="menu_section">
-                <h3>User</h3>
-                <ul class="nav side-menu">
-                    <li><a href="index.php?page=lihat-user-prodi"><i class="fa fa-laptop"></i> Prodi </a>
+            <li>
+                <a href="index.php?page=lihat-satuan"><i class="fa fa-clone"></i>Master Satuan </a>
+            </li>
+        </ul>
+    </div>
 
-                    </li>
-                    <li><a><i class="fa fa-sitemap"></i> Pegawai </a>
-
-                    </li>
-                </ul>
-            </div>
-        <?php endif; ?>
-    <?php endif; ?>
 
 </div>
