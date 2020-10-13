390fa1f93d2dc86404ef55fca97cf935edd63130
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-13 21:25:10 +0800

    #Menambahkan file proses

---
+++ web/proses.php
@@ -0,0 +1,25 @@
+<?php
+function lihatBarang()
+{
+    return "lihat barang";
+}
+
+function cariBarang()
+{
+    return "cari barang";
+}
+
+function tambahBarang()
+{
+    return "tambah barang";
+}
+
+function ubahBarang()
+{
+    return "ubah barang";
+}
+
+function hapusBarang()
+{
+    return "hapus barang";
+}
