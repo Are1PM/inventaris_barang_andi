91008ba9cfffe350f85847fc22b203d71b39d8b9
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-13 23:56:31 +0800

    #Modifikasi tampilan 404.php

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,13 @@
-<h1>PAGE NOT FOUND</h1>
+<div class="col-md-12 main_container col-sm-12 ">
+    <div class="col-md-12">
+        <div class="col-middle">
+            <div class="text-center text-center">
+                <h1 class="error-number">404</h1>
+                <h2>Maaf kami tidak dapat menemukan halaman ini</h2>
+                <p>Halaman yang anda cari tidak ada
+                </p>
+
+            </div>
+        </div>
+    </div>
+</div>
