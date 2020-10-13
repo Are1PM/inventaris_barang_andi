e492e15e77b2d037e623ee6bff4925091c49554b
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-13 15:45:00 +0800

    #buat route

--- web/routes.php
+++ web/routes.php
@@ -1,50 +1,50 @@
 <?php
-function routes($page)
+function routes($get)
 {
-    $result = PAGE . "/cek/cek.php";
+    $result = PAGES . "/";
+    $page = (isset($get['page'])) ? $get['page'] : "/";
+
+    switch ($page) {
+        case 'beranda':
+            $result .= 'beranda.php';
+            break;
+        case 'lihat-data-user-prodi':
+            $result .= "lihat-data-user-prodi.php";
+            break;
+        case 'form-input-data-user-prodi':
+            $result .= "form-input-data-user-prodi.php";
+            break;
+        case 'input-data-user-prodi':
+            $result .= "input-data-user-prodi.php";
+            break;
+        case 'edit-data-user-prodi':
+            $result .= "edit-data-user-prodi.php";
+            break;
+        case 'delete-data-user-prodi':
+            $result .= "delete-data-user-prodi.php";
+            break;
+        case 'delete-data-stok-barang':
+            $result .= "delete-stok-barang.php";
+            break;
+        case 'edit-data-stok-barang':
+            $result .= "edit-stok-barang.php";
+            break;
+        case 'form-input-stok-barang':
+            $result .= "form-input-stok-barang.php";
+            break;
+        case 'input-data-stok-barang':
+            $result .= "input-stok-barang.php";
+            break;
+        case 'lihat-data-stok-barang':
+            $result .= "lihat-stok-barang.php";
+            break;
+        case 'export-stok-pdf':
+            echo "<script language=\"JavaScript\">
+                                        document.location='export.php?file=stok-pdf';
+                                        </script>";
+            break;
+        default:
+            $result .= 'default.php';
+    }
     return $result;
 }
-
-// $page = (isset($_GET['page'])) ? $_GET['page'] : "main";
-// switch ($page) {
-//     case 'beranda':
-//         include 'beranda.php';
-//         break;
-//     case 'lihat-data-user-prodi':
-//         include "lihat-data-user-prodi.php";
-//         break;
-//     case 'form-input-data-user-prodi':
-//         include "form-input-data-user-prodi.php";
-//         break;
-//     case 'input-data-user-prodi':
-//         include "input-data-user-prodi.php";
-//         break;
-//     case 'edit-data-user-prodi':
-//         include "edit-data-user-prodi.php";
-//         break;
-//     case 'delete-data-user-prodi':
-//         include "delete-data-user-prodi.php";
-//         break;
-//     case 'delete-data-stok-barang':
-//         include "delete-stok-barang.php";
-//         break;
-//     case 'edit-data-stok-barang':
-//         include "edit-stok-barang.php";
-//         break;
-//     case 'form-input-stok-barang':
-//         include "form-input-stok-barang.php";
-//         break;
-//     case 'input-data-stok-barang':
-//         include "input-stok-barang.php";
-//         break;
-//     case 'lihat-data-stok-barang':
-//         include "lihat-stok-barang.php";
-//         break;
-//     case 'export-stok-pdf':
-//         echo "<script language=\"JavaScript\">
-//                                     document.location='export.php?file=stok-pdf';
-//                                     </script>";
-//         break;
-//     default:
-//         include '../aboutuser.php';
-// }
