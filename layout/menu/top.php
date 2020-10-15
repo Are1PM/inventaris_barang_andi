f26f57f2394d88154e02cc246ccc0ddc4cb9968c
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-15 20:27:38 +0800

    #Tambah tombol log out

--- layout/menu/top.php
+++ layout/menu/top.php
@@ -1,8 +1,11 @@
 <ul class=" navbar-right">
     <li class="nav-item dropdown open" style="padding-left: 15px;">
-        <a href="javascript:;" class="user-profile " aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+        <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
             <img src="images/user.png" alt=""><?= (isset($_SESSION['username']) ? $_SESSION['username'] : "") ?>
         </a>
+        <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+        </div>
     </li>
 
 </ul>
