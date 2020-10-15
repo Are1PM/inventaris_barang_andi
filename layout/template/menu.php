bea55ff691f6567d347a6fa90c23900b0f66bfa1
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-15 20:23:21 +0800

    #Ubah tampilan gambar user pada sidebar

--- layout/template/menu.php
+++ layout/template/menu.php
@@ -1,7 +1,10 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view">
         <div class="navbar nav_title" style="border: 0;">
-            <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+            <a href="index.html" class="site_title">
+                <i class="fa fa-paw"></i>
+                <span>Inventaris</span>
+            </a>
         </div>
 
         <div class="clearfix"></div>
@@ -9,11 +12,11 @@
         <!-- menu profile quick info -->
         <div class="profile clearfix">
             <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+                <img src="images/user.png" alt="..." class="img-circle profile_img">
             </div>
             <div class="profile_info">
                 <span>Welcome,</span>
-                <h2>John Doe</h2>
+                <h2><?= (isset($_SESSION['username']) ? $_SESSION['username'] : "") ?></h2>
             </div>
             <div class="clearfix"></div>
         </div>
