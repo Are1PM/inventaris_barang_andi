4a234020277823e7354db88aa69f28f6491e4ce0
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-16 13:46:21 +0800

    #Tambah tampilan beranda perlengkapan

---
+++ pages/perlengkapan/beranda.php
@@ -0,0 +1,14 @@
+<div class="col-md-12 col-sm-12 ">
+    <div class="x_panel">
+        <div class="x_title">
+            <h2>Data Barang Masuk</h2>
+
+            <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+
+            <canvas id="BerPer"></canvas>
+
+        </div>
+    </div>
+</div>
