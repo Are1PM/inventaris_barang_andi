ab31570b8b5544b551c2d155f75f919524ad8cb7
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-17 23:45:20 +0800

    #perbaikan layout beranda perlengkapan

--- pages/perlengkapan/beranda.php
+++ pages/perlengkapan/beranda.php
@@ -1,14 +1,24 @@
-<div class="col-md-12 col-sm-12 ">
-    <div class="x_panel">
-        <div class="x_title">
-            <h2>Data Barang Masuk Perlengkapan</h2>
+<div class="">
+    <div class="page-title">
 
-            <div class="clearfix"></div>
-        </div>
-        <div class="x_content">
+    </div>
 
-            <canvas id="BerPer"></canvas>
+    <div class="clearfix"></div>
 
+    <div class="row">
+        <div class="col-md-12 col-sm-12 ">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Data Barang Masuk Perlengkapan</h2>
+
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+
+                    <canvas id="BerPer"></canvas>
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
