5eb0335137cd49753575a8f1df9536dd7d4ab6c9
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-17 21:58:27 +0800

    #Tambah file hapus-prodi.php

---
+++ pages/perlengkapan/hapus-prodi.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['kirim'])) {
+    if (hapusProdi($_POST)) {
+        $_SESSION['pesan']['status'] = 1;
+    } else {
+        $_SESSION['pesan']['status'] = 0;
+    }
+    $_SESSION['pesan']['isi'] = "dihapus";
+}
+header('Location:index.php?page=lihat-' . $GLOBALS['currentRoute']);
