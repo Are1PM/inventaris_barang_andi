aee762adfc432b5b9daa6fd41142acb35c5f5523
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-17 22:14:47 +0800

    #Ubah hak proses

--- web/autoload.php
+++ web/autoload.php
@@ -11,7 +11,14 @@ foreach (glob('web/config/*.php') as $filename) {
     include_once $filename;
 }
 
-foreach (glob('web/proses/*.php') as $filename) {
-    include_once $filename;
+if (isset($_SESSION['username'])) {
+    if ($_SESSION['akses'] == "perlengkapan") {
+        include_once 'web/proses/perlengkapan.php';
+    } elseif ($_SESSION['akses'] == "pegawai") {
+        include_once 'web/proses/pegawai.php';
+    } elseif ($_SESSION['akses'] == "prodi") {
+        include_once 'web/proses/prodi.php';
+    }
 }
+
 include_once "routes.php";
