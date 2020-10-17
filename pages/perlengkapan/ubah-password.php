5d0a308117224dead5ab9528040787981dd21b40
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-17 22:00:15 +0800

    #Tambah file tambah-barang.php user perlengkapan

---
+++ pages/perlengkapan/ubah-password.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<div class="">
+    <div class="page-title">
+        <div class="title_left">
+            <h3><?= $judul; ?></h3>
+        </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 ">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Ubah Password</h2>
+
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card-box table-responsive">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
