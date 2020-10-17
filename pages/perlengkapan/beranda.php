792c3debf37bb9f6e36314b6162c80bcdb704b65
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-17 22:01:40 +0800

    #Update judul halaman beranda

--- pages/perlengkapan/beranda.php
+++ pages/perlengkapan/beranda.php
@@ -1,7 +1,7 @@
 <div class="col-md-12 col-sm-12 ">
     <div class="x_panel">
         <div class="x_title">
-            <h2>Data Barang Masuk</h2>
+            <h2>Data Barang Masuk Perlengkapan</h2>
 
             <div class="clearfix"></div>
         </div>
