0cca53d4fe2c063dcdb844d681be768ffa067d7b
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-17 22:07:46 +0800

    #Tambah tampilan notifikasi ke tampilan utama

--- layout/template/main.php
+++ layout/template/main.php
@@ -1,15 +1,33 @@
 <div class="right_col" role="main">
-    <div class="">
-        <div class="page-title">
-            <div class="title_left">
-                <h3>Inventaris</h3>
-            </div>
-        </div>
 
-        <div class="clearfix"></div>
+    <?php
+    if (isset($_SESSION['pesan']['status'])) {
+        if ($_SESSION['pesan']['status'] == 1) {
+            echo "<script>
+        function notifikasi(){            
+            new PNotify({
+                title: 'Sukses',
+                text: 'Data berhasil " . $_SESSION['pesan']['isi'] . "!',
+                type: 'success',
+                styling: 'bootstrap3'
+              });
+        }
+          </script>";
+        } elseif ($_SESSION['pesan']['status'] == 0) {
+            echo "<script>
+        function notifikasi(){ 
+            new PNotify({
+                title: 'Gagal',
+                text: 'Data gagal " . $_SESSION['pesan']['isi'] . "!',
+                type: 'error',
+                styling: 'bootstrap3'
+              });
+            }
+          </script>";
+        }
+        $_SESSION['pesan']['status'] = 3;
+    } ?>
 
-        <div class="row">
-            <?php include routes($_GET); ?>
-        </div>
-    </div>
+    <?php include routes($_GET); ?>
+
 </div>
