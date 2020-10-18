9e1ff3fec0603688186ecfb581581ccbdfc01df2
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-18 17:57:06 +0800

    #Tambah param file kirim barang

--- pages/perlengkapan/kirim-barang.php
+++ pages/perlengkapan/kirim-barang.php
@@ -1,10 +1,11 @@
 <?php
 if (isset($_POST['kirim'])) {
-    if (tambahBarang($_POST)) {
+    if (tambahBarang($_POST, $_FILES)) {
         $_SESSION['pesan']['status'] = 1;
     } else {
         $_SESSION['pesan']['status'] = 0;
     }
     $_SESSION['pesan']['isi'] = "ditambahkan";
 }
+
 header('Location:index.php?page=lihat-' . $GLOBALS['currentRoute']);
