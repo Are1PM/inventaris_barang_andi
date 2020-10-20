8a414084058b35d8585303cfb088c3a479056f9e
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-20 18:46:11 +0800

    #Update file kirim data untuk simpan perubahan

--- pages/perlengkapan/kirim-barang.php
+++ pages/perlengkapan/kirim-barang.php
@@ -6,6 +6,13 @@ if (isset($_POST['kirim'])) {
         $_SESSION['pesan']['status'] = 0;
     }
     $_SESSION['pesan']['isi'] = "ditambahkan";
+} elseif (isset($_POST['simpan'])) {
+    if (ubahBarang($_POST, $_FILES)) {
+        $_SESSION['pesan']['status'] = 1;
+    } else {
+        $_SESSION['pesan']['status'] = 0;
+    }
+    $_SESSION['pesan']['isi'] = "diubah";
 }
 
 header('Location:index.php?page=lihat-' . $GLOBALS['currentRoute']);
--- pages/perlengkapan/kirim-kategori.php
+++ pages/perlengkapan/kirim-kategori.php
@@ -6,5 +6,12 @@ if (isset($_POST['kirim'])) {
         $_SESSION['pesan']['status'] = 0;
     }
     $_SESSION['pesan']['isi'] = "ditambahkan";
+} elseif (isset($_POST['simpan'])) {
+    if (ubahKategori($_POST)) {
+        $_SESSION['pesan']['status'] = 1;
+    } else {
+        $_SESSION['pesan']['status'] = 0;
+    }
+    $_SESSION['pesan']['isi'] = "diubah";
 }
 header('Location:index.php?page=lihat-' . $GLOBALS['currentRoute']);
