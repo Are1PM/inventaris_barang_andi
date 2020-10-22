9536db9ab253677a5734734d9cb5ebc1bd3a4d03
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-22 17:43:23 +0800

    #update left menu pegawai

--- layout/menu/left-pegawai.php
+++ layout/menu/left-pegawai.php
@@ -7,31 +7,16 @@
                 <a href="index.php?page=beranda"><i class="fa fa-home"></i> Beranda</a>
             </li>
             <li>
-                <a href="index.php?page=lihat-barang"><i class="fa fa-edit"></i> Master Barang</a>
-            </li>
-            <li>
-                <a href="index.php?page=lihat-prodi"><i class="fa fa-bar-chart-o"></i> Master Prodi </a>
-            </li>
-            <li>
-                <a href="index.php?page=lihat-kategori"><i class="fa fa-desktop"></i> Master Kategori </a>
-            </li>
-            <li>
-                <a href="index.php?page=lihat-ruangan"><i class="fa fa-table"></i> Master Ruangan </a>
+                <a href="index.php?page=lihat-barang"><i class="fa fa-archive"></i> Data Pakai Barang</a>
             </li>
 
-            <li>
-                <a href="index.php?page=lihat-satuan"><i class="fa fa-clone"></i>Master Satuan </a>
-            </li>
         </ul>
     </div>
     <div class="menu_section">
         <h3>User</h3>
         <ul class="nav side-menu">
-            <li><a href="index.php?page=lihat-user-prodi"><i class="fa fa-laptop"></i> Prodi </a>
-
-            </li>
-            <li><a><i class="fa fa-sitemap"></i> Pegawai </a>
-
+            <li>
+                <a href="index.php?page=ubah-password"><i class="fa fa-key"></i> Ubah Password</a>
             </li>
         </ul>
     </div>
