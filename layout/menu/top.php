a7111530f62dbb7fc44ba28020a5694214f61be1
Are1PM <arwanpriantomangidi@gmail.com>
2020-10-22 17:44:21 +0800

    #Update dropdown top menu

--- layout/menu/top.php
+++ layout/menu/top.php
@@ -4,7 +4,8 @@
             <img src="images/user.png" alt=""><?= (isset($_SESSION['username']) ? $_SESSION['username'] : "") ?>
         </a>
         <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+            <a class="dropdown-item" href="?page=ubah-password"><i class="fa fa-key pull-right"></i> Ubah Password</a>
+            <a class="dropdown-item" href="?page=logout"><i class="fa fa-sign-out pull-right"></i> Keluar</a>
         </div>
     </li>
 
